038d5d06abc079b5b9781180028b44cefb007c78
AnuragRaj11 <Araj59519@gmail.com>
2025-04-09 01:03:50 +0530

    v2

--- smart_shopping/utils/data_loader.py
+++ smart_shopping/utils/data_loader.py
@@ -1,7 +1,12 @@
 import pandas as pd
+import os
+import streamlit as st
 from database.db_manager import get_connection, create_tables
 
 def load_user_data(path='data/customer_data_collection.csv'):
+    if not os.path.exists(path):
+        st.error(f"❌ Missing file: {path}")
+        return
     df = pd.read_csv(path)
     conn = get_connection()
     df.to_sql('Users', conn, if_exists='replace', index=False)
@@ -9,6 +14,9 @@ def load_user_data(path='data/customer_data_collection.csv'):
     conn.close()
 
 def load_product_data(path='data/product_recommendation_data.csv'):
+    if not os.path.exists(path):
+        st.error(f"❌ Missing file: {path}")
+        return
     df = pd.read_csv(path)
     conn = get_connection()
     df.to_sql('Products', conn, if_exists='replace', index=False)
